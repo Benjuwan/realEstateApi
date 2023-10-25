5f47508a0a25e5c6d72b7359033b195231d0fa7b
benjuwan <benjuwan.banquet12@gmail.com>
2023-10-25 17:56:44 +0900

    READMEに記述追加とページャー機能関連のコンポーネントを修正・調整.

---
+++ src/components/lists/ContentItmes.tsx
@@ -0,0 +1,42 @@
+import { memo, FC } from "react";
+import { estateInfoJsonDataContents } from "../../ts/estateInfoJsonDataContents";
+
+type ContentItemsProps = {
+    aryEl: estateInfoJsonDataContents;
+}
+
+export const ContentsItems: FC<ContentItemsProps> = memo((props) => {
+    const { aryEl } = props;
+
+    /* fee を3桁区切りに */
+    const toLocaleString = (targetWords: string) => parseInt(targetWords).toLocaleString();
+
+    return (
+        <>
+            <p>isPagerContents</p>
+            <div className="boxes categories">
+                <h2 className="type">{aryEl.Type}</h2>
+                <p className="Prefecture">{aryEl.Prefecture}</p>
+                {aryEl.Direction && <p className="direction">{aryEl.Direction}</p>}
+            </div>
+            <div className="boxes infos">
+                <p className="used">用途：{aryEl.Use}</p>
+                <p className="fee">￥{toLocaleString(aryEl.TradePrice)}</p>
+                <p className="districtName">{aryEl.Prefecture}<span>{aryEl.Municipality}</span><span>{aryEl.DistrictName}</span></p>
+                <p className="buildingYear">{aryEl.BuildingYear}</p>
+                <p className="floorPlan">{aryEl.FloorPlan}</p>
+                <p className="area">面積（平方メートル）：{aryEl.Area}</p>
+                <p className="structure">{aryEl.Structure}</p>
+                <p className="renovation">{aryEl.Renovation}</p>
+            </div>
+            <div className="boxes otherInfo">
+                <p className="purpose">目的：{aryEl.Purpose}</p>
+                <p className="period">取引時点：{aryEl.Period}</p>
+                <p className="type">取引の種類：{aryEl.Type}<span>{aryEl.CityPlanning}</span></p>
+                <p className="municipalityCode">市区町村コード：{aryEl.MunicipalityCode}</p>
+                <p className="floorAreaRatio">容積率（％）：{aryEl.FloorAreaRatio}</p>
+                <p className="coverageRatio">建ぺい率（％）：{aryEl.CoverageRatio}</p>
+            </div>
+        </>
+    );
+});
--- src/components/lists/Contents.tsx
+++ src/components/lists/Contents.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { estateInfoJsonDataContents } from "../../ts/estateInfoJsonDataContents";
 import { GetFetchDataContext } from "../../providers/lists/GetFetchData";
 import { useGetJsonData } from "../../hooks/lists/useGetJsonData";
+import { ContentsItems } from "./ContentItmes";
 
 type ContentsProps = {
     pagerLimitMaxNum?: number;
@@ -12,6 +13,14 @@ export const Contents: FC<ContentsProps> = memo((props) => {
     const { pagerLimitMaxNum } = props;
     const { isGetFetchData, isPagers, isOffSet } = useContext(GetFetchDataContext);
     const [isIndexNumStr, setIsIndexNumStr] = useState<boolean>(true);
+    const [isPagerContents, setPagerContents] = useState<estateInfoJsonDataContents[]>([]);
+    const setPagerContentsFrag = (
+        fragStart: number = isPagers,
+        fragFinish: number = isOffSet
+    ) => {
+        const splicedContents: estateInfoJsonDataContents[] = [...isGetFetchData].splice(fragStart, fragFinish);
+        setPagerContents(splicedContents);
+    }
 
     /* 都道府県内市区町村一覧取得API：https://www.land.mlit.go.jp/webland/api.html */
     const { GetJsonData } = useGetJsonData();
@@ -37,26 +46,43 @@ export const Contents: FC<ContentsProps> = memo((props) => {
             }
 
             /* ページャー処理後-A（コンテンツデータを【差し替えて】いくver）*/
-            else {
-                if (isPagers <= isOffSet) {
-                    // ページャー数がオフセットを下回っている場合は初期表示と同じ処理
-                    if (i < isOffSet) {
-                        return el;
-                    }
-                } else if (isPagers < i && i <= (isPagers + isOffSet)) {
-                    // 始点：ページャー数、終点：ページャー数 + オフセット数
+            else if (isPagers > 0) {
+                if (isPagers <= 5) {
+                    console.log(isPagers);
+                    setPagerContentsFrag(0);
+                } else {
                     if (typeof pagerLimitMaxNum !== "undefined") {
                         const nearlyLimitRange: number = pagerLimitMaxNum - isPagers;
                         if (nearlyLimitRange - isOffSet <= isOffSet) {
                             console.log(isPagers, nearlyLimitRange);
-                            if (isPagers < i && i <= pagerLimitMaxNum) {
-                                return el;
-                            }
+                            setPagerContentsFrag(isPagers, nearlyLimitRange);
                         } else {
-                            return el;
+                            console.log('else');
+                            setPagerContentsFrag();
                         }
                     }
                 }
+
+                // if (isPagers <= isOffSet) {
+                //     // ページャー数がオフセットを下回っている場合は初期表示と同じ処理
+                //     if (i < isOffSet) {
+                //         return el;
+                //     }
+                // } else if (isPagers < i && i <= (isPagers + isOffSet)) {
+                //     // 始点：ページャー数、終点：ページャー数 + オフセット数
+                //     if (typeof pagerLimitMaxNum !== "undefined") {
+                //         const nearlyLimitRange: number = pagerLimitMaxNum - isPagers;
+                //         if (nearlyLimitRange - isOffSet <= isOffSet) {
+                //             if (isPagers <= i) {
+                //                 console.log(isPagers, nearlyLimitRange);
+                //                 return el;
+                //             }
+                //         } else {
+                //             console.log(isPagers, isPagers + isOffSet);
+                //             return el;
+                //         }
+                //     }
+                // }
             }
 
             /* ページャー処理後-B（コンテンツデータを【追加・削除して】いくver）*/
@@ -79,41 +105,24 @@ export const Contents: FC<ContentsProps> = memo((props) => {
         });
     }, [isGetFetchData]); // 依存配列 isGetFetchData：コンテンツデータが取得・変更される度
 
-    /* fee を3桁区切りに */
-    const toLocaleString = (targetWords: string) => parseInt(targetWords).toLocaleString();
-
     return (
         <>
             <p style={{ 'fontSize': '16px', 'textAlign': 'center', 'marginBottom': '1em' }}>{isPagers === 0 ? isPagers + isOffSet : isPagers}件 / {pagerLimitMaxNum}</p>
             <ContentWrapper>
-                {theContents.map((el, i) => (
-                    <ContentItems key={i}>
-                        {isIndexNumStr || <p>{i + 1}</p>}
-                        <div className="boxes categories">
-                            <h2 className="type">{el.Type}</h2>
-                            <p className="Prefecture">{el.Prefecture}</p>
-                            {el.Direction && <p className="direction">{el.Direction}</p>}
-                        </div>
-                        <div className="boxes infos">
-                            <p className="used">用途：{el.Use}</p>
-                            <p className="fee">￥{toLocaleString(el.TradePrice)}</p>
-                            <p className="districtName">{el.Prefecture}<span>{el.Municipality}</span><span>{el.DistrictName}</span></p>
-                            <p className="buildingYear">{el.BuildingYear}</p>
-                            <p className="floorPlan">{el.FloorPlan}</p>
-                            <p className="area">面積（平方メートル）：{el.Area}</p>
-                            <p className="structure">{el.Structure}</p>
-                            <p className="renovation">{el.Renovation}</p>
-                        </div>
-                        <div className="boxes otherInfo">
-                            <p className="purpose">目的：{el.Purpose}</p>
-                            <p className="period">取引時点：{el.Period}</p>
-                            <p className="type">取引の種類：{el.Type}<span>{el.CityPlanning}</span></p>
-                            <p className="municipalityCode">市区町村コード：{el.MunicipalityCode}</p>
-                            <p className="floorAreaRatio">容積率（％）：{el.FloorAreaRatio}</p>
-                            <p className="coverageRatio">建ぺい率（％）：{el.CoverageRatio}</p>
-                        </div>
-                    </ContentItems>
-                ))}
+                {isPagerContents.length > 0 ?
+                    isPagerContents.map((el, i) => (
+                        <article key={i}>
+                            {isIndexNumStr || <p>{i + 1}</p>}
+                            <ContentsItems aryEl={el} />
+                        </article>
+                    )) :
+                    theContents.map((el, i) => (
+                        <article key={i}>
+                            {isIndexNumStr || <p>{i + 1}</p>}
+                            <ContentsItems aryEl={el} />
+                        </article>
+                    ))
+                }
             </ContentWrapper>
         </>
     );
@@ -123,6 +132,92 @@ const ContentWrapper = styled.div`
 width: clamp(320px, 100%, 640px);
 margin: auto;
 font-size: 1.6rem;
+
+& article {
+    border-radius: 4px;
+    padding: 1em;
+    background-color: #eaeaee;
+    margin-bottom: 4em;
+
+    & .boxes{
+        margin-bottom: 1em;
+    }
+
+    & .categories{
+        display: flex;
+        align-items: center;
+        gap: 2%;
+        line-height: 2;
+        color: #fff;
+        
+        & h2,
+        & p{
+            margin: 0;
+            font-size: 14px;
+            padding: .25em 1em;
+            border-radius: 30px;
+            background-color: limegreen;
+            text-align: center;
+        }
+
+        & p{
+            background-color: #333;
+        }
+    }
+
+    & .infos,
+    & .otherInfo{
+        line-height: 1.8;
+        font-size: 16px;
+        
+        & p{
+            margin: 0;
+            border-left: 5px solid #333;
+            padding-left: .5em;
+
+            &:not(:last-of-type){
+                margin-bottom: 1em;
+            }
+
+            & span{
+                margin: 0 .5em;
+            }
+        }
+    }
+
+    & .infos{
+        border-bottom: 1px solid #333;
+        padding-bottom: 1em;
+    }
+    & .otherInfo{
+        margin: 0;
+    }
+
+    @media screen and (min-width: 700px) {
+        display: flex;
+        flex-flow: row wrap;
+        gap: 2%;
+        width: 48%;
+
+        & .boxes{
+            width: 48%;
+        }
+
+        & .categories{
+            width: 100%;
+            margin-bottom: 1em;
+        }
+
+        & .infos{
+            border-bottom: none;
+            padding: 0;
+        }
+    }
+
+    @media screen and (min-width: 1025px) {
+        width: 32%;
+    }
+}
 
 @media screen and (min-width: 700px) {
     display: flex;
@@ -134,91 +229,5 @@ font-size: 1.6rem;
 @media screen and (min-width: 1025px) {
     width: clamp(320px, 100%, 1280px);
     font-size: 16px;
-}
-`;
-
-const ContentItems = styled.article`
-border-radius: 4px;
-padding: 1em;
-background-color: #eaeaee;
-margin-bottom: 4em;
-
-& .boxes{
-    margin-bottom: 1em;
-}
-
-& .categories{
-    display: flex;
-    align-items: center;
-    gap: 2%;
-    line-height: 2;
-    color: #fff;
-    
-    & h2,
-    & p{
-        margin: 0;
-        font-size: 14px;
-        padding: .25em 1em;
-        border-radius: 30px;
-        background-color: limegreen;
-        text-align: center;
-    }
-
-    & p{
-        background-color: #333;
-    }
-}
-
-& .infos,
-& .otherInfo{
-    line-height: 1.8;
-    font-size: 16px;
-    
-    & p{
-        margin: 0;
-        border-left: 5px solid #333;
-        padding-left: .5em;
-
-        &:not(:last-of-type){
-            margin-bottom: 1em;
-        }
-
-        & span{
-            margin: 0 .5em;
-        }
-    }
-}
-
-& .infos{
-    border-bottom: 1px solid #333;
-    padding-bottom: 1em;
-}
-& .otherInfo{
-    margin: 0;
-}
-
-@media screen and (min-width: 700px) {
-    display: flex;
-    flex-flow: row wrap;
-    gap: 2%;
-    width: 48%;
-
-    & .boxes{
-        width: 48%;
-    }
-
-    & .categories{
-        width: 100%;
-        margin-bottom: 1em;
-    }
-
-    & .infos{
-        border-bottom: none;
-        padding: 0;
-    }
-}
-
-@media screen and (min-width: 1025px) {
-    width: 32%;
 }
 `;
